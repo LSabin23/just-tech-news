636db3eae5441ecf4f41f2b15a6fc776c4717c43
unknown <44147809+LSabin23@users.noreply.github.com>
2022-01-05 21:05:32 -0500

    added Vote model, new upvote route, and model method for Post model

--- models/index.js
+++ models/index.js
@@ -1,13 +1,52 @@
 const User = require('./User')
 const Post = require('./Post')
+const Vote = require('./Vote')
 
 // create associations
+// associate one user to many posts
 User.hasMany(Post, {
   foreignKey: 'user_id'
 })
 
+// associate one post to one user
 Post.belongsTo(User, {
   foreignKey: 'user_id'
 })
 
-module.exports = { User, Post }
+// associate a user to all of their votes on many posts
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+})
+
+// associate posts a user has voted on to the user
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+})
+
+// associates a single vote to a user
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+// associate a vote to the post it was used on
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+})
+
+// associate a single user to all of their votes
+// allows us to query a user and see all the votes they've cast
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+})
+
+// associate a single post to all of its votes
+// allows us to query for a total count of votes on a single post
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Vote }
